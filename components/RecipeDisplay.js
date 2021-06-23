a6ebabb8fafc40dc6690d9a18f3559abd3a8cdc1
achen2018 <chena@brandeis.edu>
2021-06-23 09:34:33 -0400

    Added async storage in recipe list

--- components/RecipeDisplay.js
+++ components/RecipeDisplay.js
@@ -1,8 +1,74 @@
-import React, { useState } from "react";
-import {View, Text, TextInput, StyleSheet, Button, Image} from 'react-native';
+import React, { useState, useEffect } from "react";
+import {View, Text, TextInput, StyleSheet, Button, Image,
+        SafeAreaView, FlatList, ScrollView} from 'react-native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const RecipeDisplay = (props) => {
-  const [text, setText] = useState(props.name);
+  const [search, setSearch] = useState(props.name);
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+  const [recipeList, setRecipeList] = useState([])
+
+  useEffect(() => {getData()}
+           ,[])
+
+  const getData = async () => {
+       try {
+         // the '@profile_info' can be any string
+         const jsonValue = await AsyncStorage.getItem('@recipe_list')
+         let data = null
+         if (jsonValue!=null) {
+           data = JSON.parse(jsonValue)
+           setToDoItems(data)
+           console.log('just set recipe title and description')
+         } else {
+           console.log('just read a null value from Storage')
+           setInfo({})
+           setName("")
+           setEmail("")
+         }
+
+
+       } catch(e) {
+         console.log("error in getData ")
+         console.dir(e)
+         // error reading value
+       }
+  }
+
+  const storeData = async (value) => {
+       try {
+         const jsonValue = JSON.stringify(value)
+         await AsyncStorage.setItem('@todo_list', jsonValue)
+         console.log('just stored '+jsonValue)
+       } catch (e) {
+         console.log("error in storeData ")
+         console.dir(e)
+         // saving error
+       }
+  }
+
+  const clearAll = async () => {
+       try {
+         console.log('in clearData')
+         await AsyncStorage.clear()
+       } catch(e) {
+         console.log("error in clearData ")
+         console.dir(e)
+         // clear error
+       }
+  }
+
+  const renderRecipeItem = ({item}) => {
+    return(
+      <View style={{border:'thin solid blue'}}>
+        <Text>{item.title}</Text>
+        <Text>{item.description}</Text>
+      </View>
+    )
+  }
+
 
   return (
     <View>
@@ -17,13 +83,55 @@ const RecipeDisplay = (props) => {
           borderColor: 'black',
           borderWidth: 1
         }}
-        onChangeText={text => {setText(text)}}
+        onChangeText={text => {setSearch(text)}}
         defaultValue="Search Recipes"
       />
       <Button
         title='search'
         color='blue'
       />
+      <View>
+        <TextInput
+          style={{height: 20}}
+          placeholder="Enter Item"
+          onChangeText={text => {
+                setTitle(text);
+          }}
+          value={title}
+        />
+      </View>
+      <View>
+        <TextInput
+          style={{height: 20}}
+          placeholder="Enter Description"
+          onChangeText={text => {
+                setDescription(text);
+          }}
+          value={description}
+        />
+      </View>
+      <View>
+        <Button
+          title={"add"}
+          color="red"
+          onPress={() => {
+            const newRecipeList=
+            recipeList.concat(
+              {'title': title, 'description': description}
+            )
+            setRecipeList(recipeList.concat(
+              {'title': title, 'description': description}
+            ))
+            setTitle("")
+            setDescription("")
+          }}
+        />
+      </View>
+      <FlatList
+        data={recipeList}
+        renderItem={renderRecipeItem}
+        keyExtractor={item => item.title}
+      />  
     </View>
 
   )
